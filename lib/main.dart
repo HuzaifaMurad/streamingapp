5c7ba492043666c9914c1f8e4b40bdaf6916e3d1
HuzaifaMurad <hkexapril@gmail.com>
2023-02-11 23:50:18 +0500

    github is done

---
+++ lib/main.dart
@@ -0,0 +1,80 @@
+import 'package:agora_uikit/agora_uikit.dart';
+import 'package:flutter/material.dart';
+import 'package:streaming/page/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Home(),
+    );
+  }
+}
+
+
+
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({super.key, required this.title});
+
+//   // This widget is the home page of your application. It is stateful, meaning
+//   // that it has a State object (defined below) that contains fields that affect
+//   // how it looks.
+
+//   // This class is the configuration for the state. It holds the values (in this
+//   // case the title) provided by the parent (in this case the App widget) and
+//   // used by the build method of the State. Fields in a Widget subclass are
+//   // always marked "final".
+
+//   final String title;
+
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
+
+// class _MyHomePageState extends State<MyHomePage> {
+//   AgoraClient agoraClient = AgoraClient(
+//       agoraConnectionData: AgoraConnectionData(
+//           appId: 'e78fcb567cdf436389046accc0b5e2c6', channelName: 'test'),
+//       enabledPermission: [Permission.camera, Permission.microphone]);
+//   int _counter = 0;
+
+//   @override
+//   void initState() {
+//     // TODO: implement initState
+//     super.initState();
+//     agoraClient.initialize();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     // This method is rerun every time setState is called, for instance as done
+//     // by the _incrementCounter method above.
+//     //
+//     // The Flutter framework has been optimized to make rerunning build methods
+//     // fast, so that you can just rebuild anything that needs updating rather
+//     // than having to individually change instances of widgets.
+//     return Scaffold(
+//         appBar: AppBar(
+//           // Here we take the value from the MyHomePage object that was created by
+//           // the App.build method, and use it to set our appbar title.
+//           title: Text(widget.title),
+//         ),
+//         body: Stack(
+//           children: [
+//             AgoraVideoViewer(client: agoraClient),
+//             AgoraVideoButtons(client: agoraClient),
+//           ],
+//         ));
+//   }
+// }
